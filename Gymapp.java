6ea26d28c87da00ef5f5baf5342e5f4a2120a36d
Filius-Source <260125494+Filius-Source@users.noreply.github.com>
2026-02-11 13:16:11 +0100

    Add files via upload

---
+++ Gymapp.java
@@ -0,0 +1,29 @@
+public class Gymapp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Gym schema voor de hele week");
+        System.out.println("-----------------------------");
+
+        String[] days = {
+                "Maandag", "Dinsdag", "Woensdag", "Donderdag",
+                "Vrijdag", "Zaterdag", "Zondag"
+        };
+
+        String[][] exercises = {
+                { "Squat", "Bench Press", "Pull-ups", "Plank" },
+                { "Deadlift", "Overhead Press", "Barbell Row", "Hanging Leg Raises" },
+                { "Rust / Actief herstel", "Wandelen 30 min", "Stretching 15 min" },
+                { "Front Squat", "Incline Dumbbell Press", "Lat Pulldown", "Side Plank" },
+                { "Hip Thrust", "Push-ups", "Seated Cable Row", "Russian Twists" },
+                { "Cardio 30 min", "Core Circuit", "Mobility 20 min" },
+                { "Rust", "Lichte stretching 10 min" }
+        };
+
+        for (int i = 0; i < days.length; i++) {
+            System.out.println();
+            System.out.println(days[i] + ":");
+            for (int j = 0; j < exercises[i].length; j++) {
+                System.out.println("- " + exercises[i][j]);
+            }
+        }
+    }
+}
